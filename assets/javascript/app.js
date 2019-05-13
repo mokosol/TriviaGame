02e89e3cf366dc6b0a9af1773d88d7e1f14310cc
Mohamed Ali <mohamedkosol@gmail.com>
2019-05-13 10:59:22 -0500

    Hwork

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,207 +1,288 @@
-// Questions and answers Array
-var questions = [
-{
-question: 'Who won most championship in NBA?',
-answers: [
-{ answer: 'A. Laker', value: false },
-{ answer: 'B. Celtics ',value: true },
-{ answer: 'C. Knicks', value: false },
-{ answer: "D. Heat", value: false }
-]
-},
-{
-question: 'who is best shooter in NBA?',
-answers: [
+var number = 30;
 
-{ answer: 'curry', value: true },
-{ answer: 'thomson', value: false },
-{ answer: 'harden', value: false },
-{ answer: 'towns', value: false },
-]
-},
-{
- question:'who won NBA title in 2018?',
-answers: [
-{ answer: 'Warriors',value: true },
-{ answer: 'Lakers', value: false },
-{ answer: 'Thunder', value: false },
-{ answer: 'Nuggets', value: false }
-]
-},
-{
- question:'Who is all time NBA scoring leader?',
-answers: [
-{ answer: 'Kobe', value: false },
-{ answer: 'Jordan', value: false },
-{ answer: 'Magic', value: false },
-{ answer: "Kareem", value: true }
-]
-},
-{
-question: "What does 'reset' in CSS mean?",
-answers: [
-{ answer: 'Who won most NBA Title in 1990s', value: true },
-{ answer: 'Pistons', value: false },
-{ answer: 'Lakers', value: false },
-{ answer: 'Bulls', value: false }
-]
-},
-{
-question: 'How many Champions ships Lakers won?',
-answers: [
-{ answer: '15', value: false },
-{ answer: '18', value: false },
-{ answer: '16', value: true },
-{ answer: '9', value: false }
-]
-},
-{
-question: 'What NBA team Jordan play for?',
-answers: [
-{ answer: 'Bulls', value: true },
-{ answer: 'Lakers', value: false },
-{ answer: 'Celtics', value: false },
-{ answer: 'Knics', value: false }
-]
-}
-];
+var intervalId;
 
-// Global variables
-var game;
-var counter = 0;
-var clock;
-var timer = 30;
-var correctCounter = 0;
-var incorrectCounter = 0;
-var unansweredCounter = 0;
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var unanswered = 0;
 
-$(document).ready(function() {
-// Start the game when that start button is clicked
-$('.answers').css('visibility', 'hidden');
-$('body').on('click', '.start-btn', function(event) {
-event.preventDefault();
-startGame();
-$('.answers').css('visibility', 'visible');
+// Questions array
+var questions = [{
+    question: "Which album sold more records?",
+    answerList: ["Alanis Morissette's \"Jagged Little Pill\"", "Spice Girls' \"Spice\"", "Shania Twain's \"Come On Over\"", "Metallica's \"Metallica\""],
+    answer: 0
+},{
+    question: "\"Tamagotchi\" is a Japanese portmanteau of what two words?",
+    answerList: ["\"keychain\" and \"creature\"", "\"pocket\" and \"animal\"", "\"egg\" and \"watch\"", "\"handheld\" and \"clock\""],
+    answer: 2
+},{
+    question: "What was the flavor of the soft drink Surge?",
+    answerList: ["Lemon", "Lime", "Tropical", "Citrus"],
+    answer: 3
+},{
+    question: "Who sang the \"Friends\" theme song?",
+    answerList: ["Spin Doctors", "The Wallflowers", "Gin Blossoms", "The Rembrandts"],
+    answer: 3
+},{
+    question: "The 1996 Summer Olympics where held in which U.S. city?",
+    answerList: ["San Antonio", "Atlanta", "Denver", "Houston"],
+    answer: 1
+},{
+    question: "What is the name of the high school in \"Boy Meets World\"?",
+    answerList: ["John Adams High School", "John Henry High School", "John Hancock High School", "Johnny Appleseed High School"],
+    answer: 0
+},{
+    question: "When the iBook was released in 1999, which was NOT one of the five colors available?",
+    answerList: ["Tangerine", "Blueberry", "Key Lime", "Watermelon"],
+    answer: 3
+}];
+
+$("#start").on("click", function() {
+
+    // Hide Start button
+    $(this).hide();
+
+    // Display initial time countdown
+    $("#time").html("<h2>Time Remaining: 30 Seconds</h2>" + "<br>");
+
+    // Start timer countdown
+    run();
+   
+    // Display questions --- I still want to turn this into a reusable piece so that i don't have to repeat this section for each question
+    // Question 1
+    $("#question1").html("<h3>" + questions[0].question + "</h3>");
+    $("#answer1").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[0].answerList[0] + "</label>"
+        + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[0].answerList[1] + "</label>"
+        + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[0].answerList[2] + "</label>"
+        + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[0].answerList[3] + "</label><br><br>"
+    );
+
+    // Question 2
+    $("#question2").html("<h3>" + questions[1].question + "</h3>");
+    $("#answer2").html("<input type='radio' name='answer2' value='0'>" + "<label>" + questions[1].answerList[0] + "</label>"
+        + "<input type='radio' name='answer2' value='1'>" + "<label>" + questions[1].answerList[1] + "</label>"
+        + "<input type='radio' name='answer2' value='2'>" + "<label>" + questions[1].answerList[2] + "</label>"
+        + "<input type='radio' name='answer2' value='3'>" + "<label>" + questions[1].answerList[3] + "</label><br><br>"
+    );
+
+    // Question 3
+    $("#question3").html("<h3>" + questions[2].question + "</h3>");
+    $("#answer3").html("<input type='radio' name='answer3' value='0'>" + "<label>" + questions[2].answerList[0] + "</label>"
+        + "<input type='radio' name='answer3' value='1'>" + "<label>" + questions[2].answerList[1] + "</label>"
+        + "<input type='radio' name='answer3' value='2'>" + "<label>" + questions[2].answerList[2] + "</label>"
+        + "<input type='radio' name='answer3' value='3'>" + "<label>" + questions[2].answerList[3] + "</label><br><br>"
+    );
+
+    // Question 4
+    $("#question4").html("<h3>" + questions[3].question + "</h3>");
+    $("#answer4").html("<input type='radio' name='answer4' value='0'>" + "<label>" + questions[3].answerList[0] + "</label>"
+        + "<input type='radio' name='answer4' value='1'>" + "<label>" + questions[3].answerList[1] + "</label>"
+        + "<input type='radio' name='answer4' value='2'>" + "<label>" + questions[3].answerList[2] + "</label>"
+        + "<input type='radio' name='answer4' value='3'>" + "<label>" + questions[3].answerList[3] + "</label><br><br>"
+    );
+
+    // Question 5
+    $("#question5").html("<h3>" + questions[4].question + "</h3>");
+    $("#answer5").html("<input type='radio' name='answer5' value='0'>" + "<label>" + questions[4].answerList[0] + "</label>"
+        + "<input type='radio' name='answer5' value='1'>" + "<label>" + questions[4].answerList[1] + "</label>"
+        + "<input type='radio' name='answer5' value='2'>" + "<label>" + questions[4].answerList[2] + "</label>"
+        + "<input type='radio' name='answer5' value='3'>" + "<label>" + questions[4].answerList[3] + "</label><br><br>"
+    );
+    // Question 6
+    $("#question6").html("<h3>" + questions[5].question + "</h3>");
+    $("#answer6").html("<input type='radio' name='answer6' value='0'>" + "<label>" + questions[5].answerList[0] + "</label>"
+        + "<input type='radio' name='answer6' value='1'>" + "<label>" + questions[5].answerList[1] + "</label>"
+        + "<input type='radio' name='answer6' value='2'>" + "<label>" + questions[5].answerList[2] + "</label>"
+        + "<input type='radio' name='answer6' value='3'>" + "<label>" + questions[5].answerList[3] + "</label><br><br>"
+    );
+    // Question 7
+    $("#question7").html("<h3>" + questions[6].question + "</h3>");
+    $("#answer7").html("<input type='radio' name='answer7' value='0'>" + "<label>" + questions[6].answerList[0] + "</label>"
+        + "<input type='radio' name='answer7' value='1'>" + "<label>" + questions[6].answerList[1] + "</label>"
+        + "<input type='radio' name='answer7' value='2'>" + "<label>" + questions[6].answerList[2] + "</label>"
+        + "<input type='radio' name='answer7' value='3'>" + "<label>" + questions[6].answerList[3] + "</label><br><br>"
+    );
+
+    // Submit button
+    $("#submit").html("<button id='done' class='btn'>Done</button>");
+
+    // Click event runs keepingScore() and displayResults() when user clicks Done button
+    $("#done").on("click", function() {
+
+        // Keeping track of score based on correct, incorrect, and unanswered
+        keepingScore();
+
+        // Display 
+        displayResults();
+        
+    });
 });
 
-$('body').on('click', '.answer', function(event) {
-// console.log($(this));
-chosenAnswer = $(this).text();
-var answerCounter = questions[counter].answers;
+// Timer countdown function
+function run() {
 
-var answer = $('.answer');
-for (var i = 0; i < answerCounter.length; i++) {
-if (chosenAnswer === answerCounter[i].answer && answerCounter[i].value === true) {
-clearInterval(clock);
-var right = $(this).attr('class', 'right-answer answer');
-rightAnswer();
-} else if (chosenAnswer === answerCounter[i].answer && answerCounter[i].value === false) {
-clearInterval(clock);
-$(this).attr('class', 'wrong-answer answer');
-$('.first-answer').css('background-color', 'green');
-$('.first-answer').css('color', 'white');
-wrongAnswer();
-}
-}
-});
-
-$('body').on('click', '.reset-button', function(event) {
-event.preventDefault();
-resetGame();
-});
-});
-
-function rightAnswer() {
-correctCounter++;
-$('.time').html(timer);
-$('.right').html('<p>Right answers: ' + correctCounter + '</p><br>');
-setTimeout(questionCounter, 2000);
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
 }
 
-function wrongAnswer() {
-incorrectCounter++;
-$('.time').html(timer);
-$('.wrong').html('<p>Wrong answers: ' + incorrectCounter + '</p>');
-setTimeout(questionCounter, 2000);
+
+function decrement() {
+
+    //  Decrease number by one.
+    number--;
+
+    //  Show the number in the #time tag
+    $("#time").html("<h2>Time Remaining: " + number + " Seconds</h2>" + "<br>");
+
+    if (number === 0) {
+
+        // Run stop function to stop timer countdown
+        stop();
+
+        keepingScore();
+        displayResults();
+
+    }
 }
 
-function unanswered() {
-unanswered++;
-$('.main').append("<p class='times-up'>Time's up!</p>");
-$('.right-answer').css('background-color', 'green');
-$('.times-up')
-.delay(2000)
-.fadeOut(400);
-setTimeout(questionCounter, 2000);
+function stop() {
+
+    //  Clears intervalId
+    clearInterval(intervalId);
 }
 
-// Start the game
-function startGame() {
-$('.start-page').css('display', 'none');
-$('.questions-page').css('visibility', 'visible');
-$('.timer').html('<p>Time remaining: <span class="time">30</span></p>');
+// Function used for displaying results in terms of correct, incorrect, and unanswered --- I want to put all of these tags inside a div so that i can just hide the parent div
+function displayResults() {
 
-$('.question').html(questions[counter].question);
-var showingAnswers =
-'<p class="answer first-answer">' +
-questions[counter].answers[0].answer +
-'</p><p class="answer">' +
-questions[counter].answers[1].answer +
-'</p><p class="answer">' +
-questions[counter].answers[2].answer +
-'</p><p class="answer">' +
-questions[counter].answers[3].answer +
-'</p>';
+    $("#time").hide();
+    $("#question1").hide();
+    $("#answer1").hide();
+    $("#question2").hide();
+    $("#answer2").hide();
+    $("#question3").hide();
+    $("#answer3").hide();
+    $("#question4").hide();
+    $("#answer4").hide();
+    $("#question5").hide();
+    $("#answer5").hide();
+    $("#question6").hide();
+    $("#answer6").hide();
+    $("#question7").hide();
+    $("#answer7").hide();
+    $("#submit").hide();
 
-$('.answers').html(showingAnswers);
-
-timerHolder();
+    $("#resultsMessage").html("<h3>All Done!</h3>");
+    $("#correct").html("Correct Answers: " + correctAnswers);
+    $("#incorrect").html("Incorrect Answers: " + incorrectAnswers);
+    $("#unanswered").html("Unanswered: " + unanswered);
 }
 
-function questionCounter() {
-if (counter < 6) {
-counter++;
-startGame();
-timer = 30;
-timerHolder();
-} else {
-finishGame();
-}
-}
+// Function keeps score in terms of correct, incorrect, and unanswered --- I still want to make this a reusable piece so that I don't have to repeat it for each question
+function keepingScore() {
 
-// Timer function
-function timerHolder() {
-clearInterval(clock);
-clock = setInterval(seconds, 1000);
-function seconds() {
-if (timer === 0) {
-clearInterval(clock);
-unanswered();
-} else if (timer > 0) {
-timer--;
-}
-$('.time').html(timer);
-}
-}
+    var userAnswer1 = $("input[name='answer1']:checked").val();
+    var userAnswer2 = $("input[name='answer2']:checked").val();
+    var userAnswer3 = $("input[name='answer3']:checked").val();
+    var userAnswer4 = $("input[name='answer4']:checked").val();
+    var userAnswer5 = $("input[name='answer5']:checked").val();
+    var userAnswer6 = $("input[name='answer6']:checked").val();
+    var userAnswer7 = $("input[name='answer7']:checked").val();
 
-// Finishing the game
-function finishGame() {
-var final = $('.main')
-.html("<p>All done, here's how you did!<p><br><br>")
-.append('<p>Correct Answers: ' + correctCounter + '</p><br>')
-.append('<p>Wrong Answers: ' + incorrectCounter + '</p>');
-$(final).attr('<div>');
-$(final).attr('class', 'final');
-$('.final').append('<p><a class="btn btn-primary btn-lg reset-button" href="#">Restart the game!</a></p>');
-}
+    // Question 1
+    if (userAnswer1 === undefined) {
 
-// Reset the game
-function resetGame() {
-counter = 0;
-correctCounter = 0;
-incorrectCounter = 0;
-unansweredCounter = 0;
-timer = 30;
-startGame(30);
-timerHolder();
+        unanswered++;
+    }
+    else if (userAnswer1 == questions[0].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 2
+    if (userAnswer2 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer2 == questions[1].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 3
+    if (userAnswer3 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer3 == questions[2].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 4
+    if (userAnswer4 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer4 == questions[3].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 5
+    if (userAnswer5 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer5 == questions[4].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 6
+    if (userAnswer6 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer6 == questions[5].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }
+
+    // Question 7
+    if (userAnswer7 === undefined) {
+
+        unanswered++;
+    }
+    else if (userAnswer7 == questions[6].answer) {
+
+        correctAnswers++;
+    }
+    else {
+
+        incorrectAnswers++;
+    }  
 }
