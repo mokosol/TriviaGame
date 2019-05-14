3807db1c28b58aa4cc6d2c458da15cfa27cf85a9
Mohamed Ali <mohamedkosol@gmail.com>
2019-05-14 13:28:17 -0500

    HJS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,288 +1,196 @@
-var number = 30;
-
-var intervalId;
-
-var correctAnswers = 0;
-var incorrectAnswers = 0;
-var unanswered = 0;
-
-// Questions array
-var questions = [{
-    question: "Which album sold more records?",
-    answerList: ["Alanis Morissette's \"Jagged Little Pill\"", "Spice Girls' \"Spice\"", "Shania Twain's \"Come On Over\"", "Metallica's \"Metallica\""],
-    answer: 0
-},{
-    question: "\"Tamagotchi\" is a Japanese portmanteau of what two words?",
-    answerList: ["\"keychain\" and \"creature\"", "\"pocket\" and \"animal\"", "\"egg\" and \"watch\"", "\"handheld\" and \"clock\""],
-    answer: 2
-},{
-    question: "What was the flavor of the soft drink Surge?",
-    answerList: ["Lemon", "Lime", "Tropical", "Citrus"],
-    answer: 3
-},{
-    question: "Who sang the \"Friends\" theme song?",
-    answerList: ["Spin Doctors", "The Wallflowers", "Gin Blossoms", "The Rembrandts"],
-    answer: 3
-},{
-    question: "The 1996 Summer Olympics where held in which U.S. city?",
-    answerList: ["San Antonio", "Atlanta", "Denver", "Houston"],
-    answer: 1
-},{
-    question: "What is the name of the high school in \"Boy Meets World\"?",
-    answerList: ["John Adams High School", "John Henry High School", "John Hancock High School", "Johnny Appleseed High School"],
-    answer: 0
-},{
-    question: "When the iBook was released in 1999, which was NOT one of the five colors available?",
-    answerList: ["Tangerine", "Blueberry", "Key Lime", "Watermelon"],
-    answer: 3
-}];
-
-$("#start").on("click", function() {
-
-    // Hide Start button
-    $(this).hide();
-
-    // Display initial time countdown
-    $("#time").html("<h2>Time Remaining: 30 Seconds</h2>" + "<br>");
-
-    // Start timer countdown
-    run();
-   
-    // Display questions --- I still want to turn this into a reusable piece so that i don't have to repeat this section for each question
-    // Question 1
-    $("#question1").html("<h3>" + questions[0].question + "</h3>");
-    $("#answer1").html("<input type='radio' name='answer1' value='0'>" + "<label>" + questions[0].answerList[0] + "</label>"
-        + "<input type='radio' name='answer1' value='1'>" + "<label>" + questions[0].answerList[1] + "</label>"
-        + "<input type='radio' name='answer1' value='2'>" + "<label>" + questions[0].answerList[2] + "</label>"
-        + "<input type='radio' name='answer1' value='3'>" + "<label>" + questions[0].answerList[3] + "</label><br><br>"
-    );
-
-    // Question 2
-    $("#question2").html("<h3>" + questions[1].question + "</h3>");
-    $("#answer2").html("<input type='radio' name='answer2' value='0'>" + "<label>" + questions[1].answerList[0] + "</label>"
-        + "<input type='radio' name='answer2' value='1'>" + "<label>" + questions[1].answerList[1] + "</label>"
-        + "<input type='radio' name='answer2' value='2'>" + "<label>" + questions[1].answerList[2] + "</label>"
-        + "<input type='radio' name='answer2' value='3'>" + "<label>" + questions[1].answerList[3] + "</label><br><br>"
-    );
-
-    // Question 3
-    $("#question3").html("<h3>" + questions[2].question + "</h3>");
-    $("#answer3").html("<input type='radio' name='answer3' value='0'>" + "<label>" + questions[2].answerList[0] + "</label>"
-        + "<input type='radio' name='answer3' value='1'>" + "<label>" + questions[2].answerList[1] + "</label>"
-        + "<input type='radio' name='answer3' value='2'>" + "<label>" + questions[2].answerList[2] + "</label>"
-        + "<input type='radio' name='answer3' value='3'>" + "<label>" + questions[2].answerList[3] + "</label><br><br>"
-    );
-
-    // Question 4
-    $("#question4").html("<h3>" + questions[3].question + "</h3>");
-    $("#answer4").html("<input type='radio' name='answer4' value='0'>" + "<label>" + questions[3].answerList[0] + "</label>"
-        + "<input type='radio' name='answer4' value='1'>" + "<label>" + questions[3].answerList[1] + "</label>"
-        + "<input type='radio' name='answer4' value='2'>" + "<label>" + questions[3].answerList[2] + "</label>"
-        + "<input type='radio' name='answer4' value='3'>" + "<label>" + questions[3].answerList[3] + "</label><br><br>"
-    );
-
-    // Question 5
-    $("#question5").html("<h3>" + questions[4].question + "</h3>");
-    $("#answer5").html("<input type='radio' name='answer5' value='0'>" + "<label>" + questions[4].answerList[0] + "</label>"
-        + "<input type='radio' name='answer5' value='1'>" + "<label>" + questions[4].answerList[1] + "</label>"
-        + "<input type='radio' name='answer5' value='2'>" + "<label>" + questions[4].answerList[2] + "</label>"
-        + "<input type='radio' name='answer5' value='3'>" + "<label>" + questions[4].answerList[3] + "</label><br><br>"
-    );
-    // Question 6
-    $("#question6").html("<h3>" + questions[5].question + "</h3>");
-    $("#answer6").html("<input type='radio' name='answer6' value='0'>" + "<label>" + questions[5].answerList[0] + "</label>"
-        + "<input type='radio' name='answer6' value='1'>" + "<label>" + questions[5].answerList[1] + "</label>"
-        + "<input type='radio' name='answer6' value='2'>" + "<label>" + questions[5].answerList[2] + "</label>"
-        + "<input type='radio' name='answer6' value='3'>" + "<label>" + questions[5].answerList[3] + "</label><br><br>"
-    );
-    // Question 7
-    $("#question7").html("<h3>" + questions[6].question + "</h3>");
-    $("#answer7").html("<input type='radio' name='answer7' value='0'>" + "<label>" + questions[6].answerList[0] + "</label>"
-        + "<input type='radio' name='answer7' value='1'>" + "<label>" + questions[6].answerList[1] + "</label>"
-        + "<input type='radio' name='answer7' value='2'>" + "<label>" + questions[6].answerList[2] + "</label>"
-        + "<input type='radio' name='answer7' value='3'>" + "<label>" + questions[6].answerList[3] + "</label><br><br>"
-    );
-
-    // Submit button
-    $("#submit").html("<button id='done' class='btn'>Done</button>");
-
-    // Click event runs keepingScore() and displayResults() when user clicks Done button
-    $("#done").on("click", function() {
-
-        // Keeping track of score based on correct, incorrect, and unanswered
-        keepingScore();
-
-        // Display 
-        displayResults();
-        
-    });
-});
-
-// Timer countdown function
-function run() {
-
-    clearInterval(intervalId);
-    intervalId = setInterval(decrement, 1000);
-}
-
-
-function decrement() {
-
-    //  Decrease number by one.
-    number--;
-
-    //  Show the number in the #time tag
-    $("#time").html("<h2>Time Remaining: " + number + " Seconds</h2>" + "<br>");
-
-    if (number === 0) {
-
-        // Run stop function to stop timer countdown
-        stop();
-
-        keepingScore();
-        displayResults();
-
+$(document).ready(function () {
+    var options = [
+        {
+            question: "who won most NBA titles in 90s?", 
+            choice: ["Bulls", "Heat", "Lakers", "Celtics"],
+            answer: 0,
+            photo: "assets/images/MJ.gif"
+         },
+         {
+             question: "What team won NBA title 2018?", 
+            choice: ["warriors", "Spurs", "Heat", "Magic"],
+            answer: 0,
+            photo: "assets/images/curry.gif"
+         }, 
+         {
+             question: "who was final mvp in 2018?", 
+            choice: ["Lebron", "Thomson", "Curry", "Durant" ],
+            answer: 3,
+            photo: "assets/images/Durant.gif"
+        }, 
+        {
+            question: "Which player won MVP in 2018?", 
+            choice: ["Westbrook", "Harden", "Lebron", "Durant" ],
+            answer: 1,
+            photo: "assets/images/harden.gif"
+        }, 
+        {
+            question: "How many championship Lakers Won?", 
+            choice: ["12", "6", "16", "13" ],
+            answer: 2,
+            photo: "assets/images/lakers.png"
+        }, 
+        {
+            question: "What NBA team won most titles ?", 
+            choice: ["Celtics", "Lakers", "Bulls", "Spurs" ],
+            answer: 0,
+            photo: "assets/images/celtics.jpg"
+        }, 
+        {
+            question: "Who was best player in 80s?", 
+            choice: ["Magic", "Jordan", "Bird", "Kobe" ],
+            answer: 0,
+            photo: "assets/images/magic.jpg"
+        }, 
+        {
+            question: "Who won mos titles in 80s?", 
+            choice: ["Knicks", "Celtics", "Lakers", "Spurs" ],
+            answer: 2,
+            photo: "assets/images/lakers.png"
+        }];
+    
+    var correctCount = 0;
+    var wrongCount = 0;
+    var unanswerCount = 0;
+    var timer = 20;
+    var intervalId;
+    var userGuess ="";
+    var running = false;
+    var qCount = options.length;
+    var pick;
+    var index;
+    var newArray = [];
+    var holder = [];
+    
+    
+    
+    $("#reset").hide();
+    //click start button to start game
+    $("#start").on("click", function () {
+            $("#start").hide();
+            displayQuestion();
+            runTimer();
+            for(var i = 0; i < options.length; i++) {
+        holder.push(options[i]);
     }
-}
-
-function stop() {
-
-    //  Clears intervalId
-    clearInterval(intervalId);
-}
-
-// Function used for displaying results in terms of correct, incorrect, and unanswered --- I want to put all of these tags inside a div so that i can just hide the parent div
-function displayResults() {
-
-    $("#time").hide();
-    $("#question1").hide();
-    $("#answer1").hide();
-    $("#question2").hide();
-    $("#answer2").hide();
-    $("#question3").hide();
-    $("#answer3").hide();
-    $("#question4").hide();
-    $("#answer4").hide();
-    $("#question5").hide();
-    $("#answer5").hide();
-    $("#question6").hide();
-    $("#answer6").hide();
-    $("#question7").hide();
-    $("#answer7").hide();
-    $("#submit").hide();
-
-    $("#resultsMessage").html("<h3>All Done!</h3>");
-    $("#correct").html("Correct Answers: " + correctAnswers);
-    $("#incorrect").html("Incorrect Answers: " + incorrectAnswers);
-    $("#unanswered").html("Unanswered: " + unanswered);
-}
-
-// Function keeps score in terms of correct, incorrect, and unanswered --- I still want to make this a reusable piece so that I don't have to repeat it for each question
-function keepingScore() {
-
-    var userAnswer1 = $("input[name='answer1']:checked").val();
-    var userAnswer2 = $("input[name='answer2']:checked").val();
-    var userAnswer3 = $("input[name='answer3']:checked").val();
-    var userAnswer4 = $("input[name='answer4']:checked").val();
-    var userAnswer5 = $("input[name='answer5']:checked").val();
-    var userAnswer6 = $("input[name='answer6']:checked").val();
-    var userAnswer7 = $("input[name='answer7']:checked").val();
-
-    // Question 1
-    if (userAnswer1 === undefined) {
-
-        unanswered++;
+        })
+    //timer start
+    function runTimer(){
+        if (!running) {
+        intervalId = setInterval(decrement, 1000); 
+        running = true;
+        }
     }
-    else if (userAnswer1 == questions[0].answer) {
-
-        correctAnswers++;
+    //timer countdown
+    function decrement() {
+        $("#timeleft").html("<h3>Time remaining: " + timer + "</h3>");
+        timer --;
+    
+        //stop timer if reach 0
+        if (timer === 0) {
+            unanswerCount++;
+            stop();
+            $("#answerblock").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+            hidepicture();
+        }	
     }
-    else {
-
-        incorrectAnswers++;
+    
+    //timer stop
+    function stop() {
+        running = false;
+        clearInterval(intervalId);
     }
-
-    // Question 2
-    if (userAnswer2 === undefined) {
-
-        unanswered++;
+    //randomly pick question in array if not already shown
+    //display question and loop though and display possible answers
+    function displayQuestion() {
+        //generate random index in array
+        index = Math.floor(Math.random()*options.length);
+        pick = options[index];
+    
+    //	if (pick.shown) {
+    //		//recursive to continue to generate new index until one is chosen that has not shown in this game yet
+    //		displayQuestion();
+    //	} else {
+    //		console.log(pick.question);
+            //iterate through answer array and display
+            $("#questionblock").html("<h2>" + pick.question + "</h2>");
+            for(var i = 0; i < pick.choice.length; i++) {
+                var userChoice = $("<div>");
+                userChoice.addClass("answerchoice");
+                userChoice.html(pick.choice[i]);
+                //assign array position to it so can check answer
+                userChoice.attr("data-guessvalue", i);
+                $("#answerblock").append(userChoice);
+    //		}
     }
-    else if (userAnswer2 == questions[1].answer) {
-
-        correctAnswers++;
+    
+    
+    
+    //click function to select answer and outcomes
+    $(".answerchoice").on("click", function () {
+        //grab array position from userGuess
+        userGuess = parseInt($(this).attr("data-guessvalue"));
+    
+        //correct guess or wrong guess outcomes
+        if (userGuess === pick.answer) {
+            stop();
+            correctCount++;
+            userGuess="";
+            $("#answerblock").html("<p>Correct!</p>");
+            hidepicture();
+    
+        } else {
+            stop();
+            wrongCount++;
+            userGuess="";
+            $("#answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+            hidepicture();
+        }
+    })
     }
-    else {
-
-        incorrectAnswers++;
+    
+    
+    function hidepicture () {
+        $("#answerblock").append("<img src=" + pick.photo + ">");
+        newArray.push(pick);
+        options.splice(index,1);
+    
+        var hidpic = setTimeout(function() {
+            $("#answerblock").empty();
+            timer= 20;
+    
+        //run the score screen if all questions answered
+        if ((wrongCount + correctCount + unanswerCount) === qCount) {
+            $("#questionblock").empty();
+            $("#questionblock").html("<h3>Game Over!  Here's how you did: </h3>");
+            $("#answerblock").append("<h4> Correct: " + correctCount + "</h4>" );
+            $("#answerblock").append("<h4> Incorrect: " + wrongCount + "</h4>" );
+            $("#answerblock").append("<h4> Unanswered: " + unanswerCount + "</h4>" );
+            $("#reset").show();
+            correctCount = 0;
+            wrongCount = 0;
+            unanswerCount = 0;
+    
+        } else {
+            runTimer();
+            displayQuestion();
+    
+        }
+        }, 3000);
+    
+    
     }
-
-    // Question 3
-    if (userAnswer3 === undefined) {
-
-        unanswered++;
-    }
-    else if (userAnswer3 == questions[2].answer) {
-
-        correctAnswers++;
-    }
-    else {
-
-        incorrectAnswers++;
-    }
-
-    // Question 4
-    if (userAnswer4 === undefined) {
-
-        unanswered++;
-    }
-    else if (userAnswer4 == questions[3].answer) {
-
-        correctAnswers++;
-    }
-    else {
-
-        incorrectAnswers++;
-    }
-
-    // Question 5
-    if (userAnswer5 === undefined) {
-
-        unanswered++;
-    }
-    else if (userAnswer5 == questions[4].answer) {
-
-        correctAnswers++;
-    }
-    else {
-
-        incorrectAnswers++;
-    }
-
-    // Question 6
-    if (userAnswer6 === undefined) {
-
-        unanswered++;
-    }
-    else if (userAnswer6 == questions[5].answer) {
-
-        correctAnswers++;
-    }
-    else {
-
-        incorrectAnswers++;
-    }
-
-    // Question 7
-    if (userAnswer7 === undefined) {
-
-        unanswered++;
-    }
-    else if (userAnswer7 == questions[6].answer) {
-
-        correctAnswers++;
-    }
-    else {
-
-        incorrectAnswers++;
-    }  
-}
+    
+    $("#reset").on("click", function() {
+        $("#reset").hide();
+        $("#answerblock").empty();
+        $("#questionblock").empty();
+        for(var i = 0; i < holder.length; i++) {
+            options.push(holder[i]);
+        }
+        runTimer();
+        displayQuestion();
+    
+    })
+    
+    })
